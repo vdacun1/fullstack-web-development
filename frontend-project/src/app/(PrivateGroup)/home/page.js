d345128a03a6609aafd486e16fb47b87505b6e9f
Victor Da Cunha Nuñez <134562308+vdacun1@users.noreply.github.com>
2024-03-11 17:41:50 -0300

    Some changes were made in layout.js to test the workflows setted on github (#3)
    MVP de autenticación

---
+++ frontend-project/src/app/(PrivateGroup)/home/page.js
@@ -0,0 +1,7 @@
+export default async function LandingPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>You are logged in!</h1>
+    </main>
+  );
+}
