4c6a575a37de96119ba66bde6b5da5fb7a2fd489
vdacun1 <vdacun1@palermo.edu>
2024-03-27 14:21:02 -0300

    Workflow

--- test/integration/user/register.test.js
+++ test/integration/user/register.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../../../src/app");
 
 describe("POST /user/register", () => {
-  test("It should respond with a 200 status code", async () => {
+  test("Should create the user successfully", async () => {
     const response = await request(app).post("/user/register").send({
       email: "user@gmail.com",
       password: "password",
@@ -10,16 +10,18 @@ describe("POST /user/register", () => {
 
     console.log(response.body);
     expect(response.statusCode).toBe(201);
-    expect(response.body).toHaveProperty("email");
+    expect(response.body.status).toBe(201);
+    expect(response.body.message).toBe("User registered successfully");
   });
 
-  test("It should not respond with a 200 status code", async () => {
+  test("Should not create user due to conflict", async () => {
     const response = await request(app).post("/user/register").send({
       email: "user+123@gmail.com",
       password: "password",
     });
 
     expect(response.statusCode).toBe(409);
+    expect(response.body.status).toBe(409);
     expect(response.body.message).toBe("User already exists");
   });
 });
