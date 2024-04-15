b9b4dbe985867e37ddcb813c6b7d7a290ce2d357
vdacun1 <vdacun1@palermo.edu>
2024-04-15 15:39:50 -0300

    A limiter has been implemented in confirm-email due to CodeQL: This route handler performs, but is not rate-limited.

--- src/presentation/routes/user.js
+++ src/presentation/routes/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const RegisterRequest = require('../../application/requests/RegisterRequest');
 const ConfirmEmailUseCase = require('../../application/usecases/ConfirmEmailUseCase');
 
+const { limiter } = require('../../infrastructure/Config');
+
 const user = express.Router();
 
 user.post('/register', RegisterRequest.validate(), RegisterRequest.handle);
@@ -10,6 +12,10 @@ user.post('/forgot-password', async (req, res) => {
   return res.send('Forgot password');
 });
 
-user.get('/confirm-email/:email_verification_code', ConfirmEmailUseCase.handle);
+user.get(
+  '/confirm-email/:email_verification_code',
+  limiter,
+  ConfirmEmailUseCase.handle,
+);
 
 module.exports = user;
